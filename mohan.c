d7f545d2510fee3dbc34747aab9ab2483e00ccf7
MohanMargana <92287913+MohanMargana@users.noreply.github.com>
2021-10-20 10:52:14 +0530

    Add files via upload

---
+++ mohan.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//Variable Declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//Initialization
+	age=19;
+	cgpa=7.2;
+	initial='M';
+	
+	//Output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c",initial);	
+}
