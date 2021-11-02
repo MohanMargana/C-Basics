7441bf1c4a65e666fd7c681206f3d7c8072356fe
MohanMargana <92287913+MohanMargana@users.noreply.github.com>
2021-11-02 10:31:23 +0530

    Add files via upload

--- mohan.c
+++ mohan.c
@@ -1,23 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-	//variable declaration;
-	int phy, che, mat, eng, tot;
+	int n,nat,even,odd;
+	n = 5;
+	nat = n*(n+1)/2;
+	even = n*(n+1);
+	odd = n*n;
 	
-	//initiallisation;
-	phy = 60;
-	che = 50;
-	mat = 70;
-	eng = 40;
-
-	//calculating;
-	tot = phy+che+mat+eng;
-
-	//output;
-	printf("marks in physics: %d\n", phy);
-	printf("marks in chemistry: %d\n", che);
-	printf("marks in maths: %d\n", mat);
-	printf("marks in english: %d\n", eng);
-	printf("total: %d", tot);
+	printf("sum of %d natural numbers = %d\n",n,nat);
+	printf("sum of %d natural numbers = %d\n",n,even);
+	printf("sum of %d natural numbers = %d\n",n,odd);
 	
-}  
+}
