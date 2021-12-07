b6bc1bc87659c91b3fee8671f0bd162eb86799f1
MohanMargana <92287913+MohanMargana@users.noreply.github.com>
2021-12-07 10:16:05 +0530

    Add files via upload

---
+++ if and else 333.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int s,l;
+	printf("enter the number");
+	scanf("%d %d",&s,&l);
+	if(s<l)
+	{
+		printf("%d is the largest number",l);
+	}
+	else
+	{
+		printf("%d is the largest number",s);
+	}
+}
+	
