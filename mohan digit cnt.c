69e25e2abfe6ed1b2474c48639499fd39e7bc52b
MohanMargana <92287913+MohanMargana@users.noreply.github.com>
2022-04-03 22:41:43 -0700

    Add files via upload

---
+++ mohan digit cnt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,r,edc=0,odc=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		if(r%2==0)
+		{
+			edc++;
+		}
+		else
+		{
+			odc++;
+		}
+		n=n/10;
+	}
+	printf("odd digits: %d\n",odc);
+	printf("even digits: %d",edc);
+}
